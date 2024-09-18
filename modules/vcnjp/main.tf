f5e574b0d20658b090059a7130569c881fa71a92
jean-philippe.breysse <jeanphi@jean-philippes-MacBook-Air.local>
2024-09-18 14:49:50 +0200

    6th  commit

--- modules/vcnjp/main.tf
+++ modules/vcnjp/main.tf
@@ -4,3 +4,13 @@ resource "oci_core_vcn" "internal" {
   compartment_id = var.compartment_id
   display_name   = "My internal VCN"
 }
+
+
+resource "oci_core_subnet" "internal" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "10.0.1.0/24"
+  compartment_id             = var.compartment_id
+  display_name               = "Dev subnet "
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
+}
